9c72c772a051c69b98a24dab1eaf61e620c008c8
FelixFifi <FelixFifi@users.noreply.github.com>
2023-10-10 17:11:54 +0200

    Show race completion time Prevent dead ships from collecting race checkpoints

--- CheckpointsLabel.gd
+++ CheckpointsLabel.gd
@@ -3,15 +3,17 @@ extends Label
 var race_controller
 
 const LABEL_FORMAT = "Checkpoints: {current}/{goal}"
-const LABEL_FORMAT_WON = LABEL_FORMAT + "  YOU WON!"
+const LABEL_FORMAT_WON = "Race completed in {time}s!"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	race_controller = owner.get_race_controller()
 	race_controller.checkpoint_hit.connect(_on_race_controller_checkpoint_hit)
+	race_controller.race_completed.connect(_on_race_completed)
 
 func _on_race_controller_checkpoint_hit(current, goal):
-	var label_format = LABEL_FORMAT if current < goal else LABEL_FORMAT_WON
+	text = LABEL_FORMAT.format({"current": current, "goal": goal})
 
-	text = label_format.format({"current": current, "goal": goal})
+func _on_race_completed(time):
+	text = LABEL_FORMAT_WON.format({"time": "%0.2f" % time})
--- RaceController.gd
+++ RaceController.gd
@@ -2,15 +2,18 @@ extends Node3D
 
 var checkpoints: Dictionary = {}
 var checkpoints_reached := 0
+var race_start: float
 
 @onready var spawn_point = %SpawnPoint
 
 signal checkpoint_hit(current, goal)
+signal race_completed(time)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_load_checkpoints()
 	spawn_point.ship_respawned.connect(_on_spawnpoint_ship_respawned)
+	_reset()
 
 func _load_checkpoints():
 	for child in get_children():
@@ -20,6 +23,7 @@ func _load_checkpoints():
 
 func _reset():
 	checkpoints_reached = 0
+	race_start = Time.get_unix_time_from_system()
 
 	for key in checkpoints:
 		checkpoints[key] = false
@@ -29,9 +33,17 @@ func _reset():
 func _on_spawnpoint_ship_respawned(_new_ship):
 	_reset()
 
-func _on_checkpoint_entered(body: Node3D, checkpoint: Area3D):
+func _on_checkpoint_entered(ship: Ship, checkpoint: Area3D):
+	if ship == null or ship.dead:
+		return
 	if !checkpoints[checkpoint]:
 		checkpoints[checkpoint] = true
 		checkpoints_reached += 1
 		checkpoint_hit.emit(checkpoints_reached, checkpoints.size())
+
+	if checkpoints_reached == checkpoints.size():
+		var race_end := Time.get_unix_time_from_system()
+
+		race_completed.emit(race_end - race_start)
+
 
